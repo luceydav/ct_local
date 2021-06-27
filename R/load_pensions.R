56873bf6f59ecafa9d6b2b8c11ffb04938c9b3d6
luceydav <dnl2001@stern.nyu.edu>
2021-06-27 06:15:19 -0400

    add 2019 pensions

--- R/load_pensions.R
+++ R/load_pensions.R
@@ -6,54 +6,80 @@ load_pension <- function(){
   
   #Extract pension data from ct_mfi file
   ct_mfi_pension <-
-    do.call("c" , lapply(list.files(subdir, full.names = TRUE), extract_pension))
+    do.call("c" , lapply(list.files(subdir, pattern = "mdb", full.names = TRUE), extract_pension))
   names <- names(ct_mfi_pension)
   
   #Eliminate labels by converting to matrix then data.table
   ct_mfi_pension <- lapply(ct_mfi_pension, as.matrix)
   ct_mfi_pension <- lapply(ct_mfi_pension, as.data.table)
-  names(ct_mfi_pension) <- names
   
+  # Add current year
+  socrata_pension <- 
+    fread("https://data.ct.gov/resource/civu-w22d.csv")
+  names(ct_mfi_pension) <- names
+  ct_mfi_pension[["2019"]] <- socrata_pension
+
   #Add all years back together in data.table names by year
   ct_mfi_pension <-
     rbindlist(ct_mfi_pension,
               use.names = TRUE,
               fill = TRUE,
-              idcol = "year")
+              idcol = "fisc_year_end")
   ct_mfi_pension <-
-    unique(ct_mfi_pension, by = c("year", "entity", "plan_name"))
-  setnames(ct_mfi_pension,
-           c("entity", "year"),
-           c("municipality", "fisc_year_end"))
+    unique(ct_mfi_pension, by = c("fisc_year_end", "entity", "plan_name"))
+  ct_mfi_pension[, municipality := fcoalesce(town, entity)]
+  
+  # Convert needed vars to integer
+  vars <- c(
+    "req_d_emplyr_contrib",
+    "emplyr_required_adc_gasb67",
+    "emplyr_required_adc",
+    "emplyr_contrib_made",
+    "emplyr_contribution_gasb67",
+    "emplyr_contribution",
+    "gasb5_net_assets",
+    "gasb27_asset_value",
+    "plan_fiduciary_net_position_gasb6768",
+    "plan_fiduciary_net_position",
+    "gasb27_aal",
+    "total_pension_liability_gasb6768",
+    "total_pension_liability"
+  )
+  ct_mfi_pension[, (vars) := lapply(.SD, as.integer), .SDcols = vars]
   
   #Coalesce variables which have different names in different annual tables
   ct_mfi_pension <-
     ct_mfi_pension[, `:=`
-                   (
-                     emplyr_cont_req =
+                   ( emplyr_cont_req =
                        as.numeric(
                          fcoalesce(req_d_emplyr_contrib,
-                                   emplyr_required_adc_gasb67)
+                                   emplyr_required_adc_gasb67,
+                                   emplyr_required_adc)
                        ),
                      emplyr_cont_made =
                        as.numeric(
                          fcoalesce(emplyr_contrib_made,
-                                   emplyr_contribution_gasb67)
+                                   emplyr_contribution_gasb67,
+                                   emplyr_contribution)
                        ),
                      pension_net_ass =
                        as.numeric(
                          fcoalesce(
                            gasb5_net_assets,
                            gasb27_asset_value,
-                           plan_fiduciary_net_position_gasb6768
+                           plan_fiduciary_net_position_gasb6768,
+                           plan_fiduciary_net_position
                          )
                        ),
                      pension_liab = as.numeric(
                        fcoalesce(gasb27_aal,
-                                 total_pension_liability_gasb6768)
+                                 total_pension_liability_gasb6768,
+                                 total_pension_liability)
                      ),
                      number_of_members = as.numeric(number_of_members),
-                     inv_returns = as.numeric(investment_rate_of_return_gasb6768),
+                     inv_returns = fcoalesce(
+                       as.numeric(investment_rate_of_return_gasb6768),
+                       investment_rate_of_return),
                      municipality = str_to_title(municipality)
                    )]
   
@@ -80,18 +106,24 @@ load_pension <- function(){
     "pension_net_ass"
   )
   pension_data <-
-    ct_mfi_pension[, lapply(.SD, sum, na.rm = TRUE),
-                   .SDcols = cols,
-                   .(fisc_year_end, municipality)]
+    ct_mfi_pension[, 
+      lapply(.SD, sum, na.rm = TRUE),
+      .SDcols = cols,
+      .(fisc_year_end, municipality)]
   
-  #Aggregate mean by town/year
+  #Aggregate mean by municipality/fisc_year_end
+  ct_mfi_pension[, inv_returns := fifelse(inv_returns > 1, inv_returns, inv_returns * 100)]
   inv_returns <-
-    ct_mfi_pension[, (mean(inv_returns, na.rm = TRUE) * 100),
-                   .(fisc_year_end, municipality)]
+    ct_mfi_pension[,
+      .( inv_returns = 
+          mean(inv_returns, na.rm = TRUE)
+         ),
+      .(fisc_year_end, municipality)]$inv_returns
   
   #Rebuild with town/year aggregations
-  ct_mfi_pension <- data.table(pension_data,
-                               inv_returns = inv_returns$V1)
+  ct_mfi_pension <- 
+    data.table(pension_data,
+               inv_returns = inv_returns)
   
   ct_mfi_pension_DT <-
     ct_mfi_pension[, .(
